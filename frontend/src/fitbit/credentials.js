3493eade121c6eebd09e4c9651c8267395f72c60
anthonyriz <144366780+anthonyriz@users.noreply.github.com>
2024-03-30 12:07:44 -0400

    credentials for fitbit

---
+++ frontend/src/fitbit/credentials.js
@@ -0,0 +1,5 @@
+const CLIENT_ID = '23RZM3';
+const CLIENT_SECRET = '2679e383117b602b2b1ae44d017bfdc1';
+const REDIRECT_URI = 'https://healthmonitoringapp-e67d3.web.app/'
+
+export {CLIENT_ID, CLIENT_SECRET, REDIRECT_URI};
