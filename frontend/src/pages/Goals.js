c44ffb4d31162ca4bae695c2c3e22836f9b0fd8f
AlanAtrach1 <aatrach@stevens.edu>
2024-04-22 15:42:20 -0400

    implementing slider, now correctly displays current value

--- frontend/src/pages/Goals.js
+++ frontend/src/pages/Goals.js
@@ -12,19 +12,25 @@ const Goals = () => {
     );
 };
 
-const MainSection = () => {
-    const [sleepValue, setSleep] = useState(8)
-    const [stepValue, setStep] = useState(5000)
-    const [calValue, setCal] = useState(2000)
 
-    const sleepChange = (event) => {
-        setSleep(event.target.sleepValue)
+
+
+
+const MainSection = () => {
+    const [sleepValue, setSleepValue] = useState(8);
+    const [stepValue, setStepValue] = useState(5000);
+    const [calValue, setCalvalue] = useState(2000);
+
+    function handleSleepChange(event) {
+        setSleepValue(parseInt(event.target.value))
     };
-    const stepChange = (event) => {
-        setStep(event.target.stepValue)
+
+    const handleStepChange = (event) => {
+        setStepValue(parseInt(event.target.value));
     };
-    const calChange = (event) => {
-        setCal(event.target.calValue)
+
+    const handleCalChange = (event) => {
+        setCalvalue(parseInt(event.target.value));
     };
 
     return (
@@ -47,7 +53,7 @@ const MainSection = () => {
                                         max={12}
                                         step={1}
                                         label="Set Sleep Goal:"
-                                        handleChange={sleepChange} />
+                                        changeHandler={handleSleepChange} />
                                     <h3>{sleepValue} hours</h3>
                                     <button type="button" className="btn btn-primary btn">Set Goal</button>
                                 </form>
@@ -62,13 +68,13 @@ const MainSection = () => {
                                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                                 <form>
                                     <Slider
-                                        value={8}
-                                        min={1}
-                                        max={12}
-                                        step={1}
-                                        label="Set Sleep Goal:"
-                                        handleChange={stepChange} />
-                                    <h3>{stepValue} hours</h3>
+                                        value={stepValue}
+                                        min={500}
+                                        max={20000}
+                                        step={500}
+                                        label="Set Steps Goal:"
+                                        changeHandler={handleStepChange} />
+                                    <h3>{stepValue} steps</h3>
                                     <button type="button" className="btn btn-primary btn">Set Goal</button>
                                 </form>
                             </div>
@@ -82,13 +88,13 @@ const MainSection = () => {
                                 <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. </p>
                                 <form>
                                     <Slider
-                                        value={8}
-                                        min={1}
-                                        max={12}
-                                        step={1}
-                                        label="Set Sleep Goal:"
-                                        handleChange={calChange} />
-                                    <h3>{calValue} hours</h3>
+                                        value={calValue}
+                                        min={1000}
+                                        max={6000}
+                                        step={100}
+                                        label="Set Calorie Goal:"
+                                        changeHandler={handleCalChange} />
+                                    <h3>{calValue} calories</h3>
                                     <button type="button" className="btn btn-primary btn">Set Goal</button>
                                 </form>
                             </div>
